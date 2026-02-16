7f31f73176feec1e404d9cf579e3c3c8fdf0f645
Andrey <vstup230921@gmail.com>
2026-02-16 18:31:58 +0200

    feat: admin settings for user model

--- mcenter/users/admin.py
+++ mcenter/users/admin.py
@@ -1,2 +1,29 @@
+from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "surname",
+        "role",
+        "email",
+        "phone",
+    )
+    search_fields = (
+        "^first_name",
+        "^last_name",
+        "^username",
+        "^email",
+        "phone",
+    )
+    list_filter = ("role",)
+    ordering = ("-id",)
+    save_on_top = True
+
+    @admin.display(description="Full name")
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name} {obj.surname}".strip()
