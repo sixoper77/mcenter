cc9e4cc0fd3ff7b64b06c2c2f9f0e15d78d02e67
Andrey <vstup230921@gmail.com>
2026-02-20 22:32:31 +0200

    feat: admin panels for clinic,doctor and appointment

--- mcenter/appointment/admin.py
+++ mcenter/appointment/admin.py
@@ -1,2 +1,69 @@
+from django.contrib import admin
 
-# Register your models here.
+from .models import Appointment, Clinic, Doctor
+
+
+@admin.register(Clinic)
+class ClinicAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "legal_address",
+        "physical_address",
+    ]
+    search_fields = [
+        "^name",
+    ]
+    list_filter = [
+        "name",
+    ]
+    ordering = ("-id",)
+    save_on_top = True
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = [
+        "doctor",
+        "specialization",
+        "clinic",
+    ]
+    search_fields = [
+        "doctor",
+        "^specialization",
+        "clinic",
+    ]
+    list_filter = [
+        "specialization",
+        "clinic",
+    ]
+    ordering = ("-id",)
+    save_on_top = True
+
+    @admin.display(description="Клинники")
+    def get_clinics(self, obj):
+        return ", ".join([clinic.name for clinic in obj.clinic.all()])
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = [
+        "doctor",
+        "patient",
+        "clinic",
+        "status",
+        "timestamp",
+    ]
+    search_fields = [
+        "doctor__doctor__username",
+        "patient__username",
+        "clinic__name",
+        "status",
+    ]
+    list_filter = [
+        "doctor",
+        "clinic",
+        "status",
+        "created_at",
+    ]
+    ordering = ("-id",)
+    save_on_top = True
